3ea35a781110157325cc537f51dac0c786f32eb4
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-13 19:47:43 +0100

    adds image for artist

--- src/components/musicItemDetail.js
+++ src/components/musicItemDetail.js
@@ -4,6 +4,7 @@ export default function MusicItemDetail(props) {
     return (
         <div>
             <p>{props.index}</p>
+            <img src={props.song["im:image"][0].label} />
             <p>{props.song.title.label}</p>
         </div>
     )
