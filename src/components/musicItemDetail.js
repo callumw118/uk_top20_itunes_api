adaa1b835355cd0a69ee6b5fbe6ce21cb8d40021
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-13 19:51:41 +0100

    adds audio previous clip

--- src/components/musicItemDetail.js
+++ src/components/musicItemDetail.js
@@ -6,6 +6,10 @@ export default function MusicItemDetail(props) {
             <p>{props.index}</p>
             <img src={props.song["im:image"][0].label} />
             <p>{props.song.title.label}</p>
+            <audio 
+                controls
+                src={props.song.link[1].attributes.href}>
+            </audio>
         </div>
     )
 }
